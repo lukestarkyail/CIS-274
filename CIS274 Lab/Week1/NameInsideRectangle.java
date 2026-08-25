76c4e18a1224136ba2e2e1eeb431493e8e6813a9
lukestarkyail <lstar8545@gmail.com>
2026-08-24 17:25:38 -0700

    Week1: NameInsideRectangle

---
+++ CIS274 Lab/Week1/NameInsideRectangle.java
@@ -0,0 +1,16 @@
+// Luke La
+// Aug 19, 2026
+// Lab 1
+// This program print out my name inside a rectangle
+public class NameInsideRectangle
+{
+    public static void main(String[] args)
+    {
+        System.out.println("############");
+        System.out.println("#          #");
+        System.out.println("# Luke La  #");
+        System.out.println("#          #");
+        System.out.println("############");
+
+    }
+}
